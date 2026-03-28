77ed427843e43e79f6543358993cf660b7b2f46a
Vy Hong <vy.hong@dsit.gov.uk>
2026-03-28 18:01:18 +0000

    REmove old files

--- main-pipeline/pipeline.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S uv run python
-"""Entry point for shallow-review pipeline."""
-
-from shallow_review.cli import main
-
-if __name__ == "__main__":
-    main()
-
-
--- main-pipeline/shallow_review/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Shallow review data pipeline and research tools."""
-
-__version__ = "0.1.0"
-
-__all__ = ["__version__"]
-
-
